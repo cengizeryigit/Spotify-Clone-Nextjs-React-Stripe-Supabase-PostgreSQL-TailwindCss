3575df7e034fcbdc6d240e1f30b173ec351ff31e
Cengiz <cengiz_eryigit@hotmail.com>
2024-02-29 16:57:49 +0300

    Authentication modal and functionality

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -21,51 +21,38 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
     <button
       onClick={onClick}
       className="
-        relative
-        group
-        flex
-        items-center
-        rounded-md
-        overflow-hidden
-        gap-x-4
-        bg-neutral-100/10
-        hover:bg-neutral-100/20
-        transition
+        relative 
+        group 
+        flex 
+        items-center 
+        rounded-md 
+        overflow-hidden 
+        gap-x-4 
+        bg-neutral-100/10 
+        cursor-pointer 
+        hover:bg-neutral-100/20 
+        transition 
         pr-4
       "
     >
-      <div
-        className="
-            relative
-            min-h-[64px]
-            min-w-[64px]
-          "
-      >
-        <Image className="object-cover" fill src={image} alt="Image" />
+      <div className="relative min-h-[64px] min-w-[64px]">
+        <Image className="object-cover" src={image} fill alt="Image" />
       </div>
-      <p
-        className="
-          font-medium
-          truncate
-          py-5
-        "
-      >
-        {name}
-      </p>
+      <p className="font-medium truncate py-5">{name}</p>
       <div
         className="
-          absolute
-          transition
-          opacity-0
-          rounded-full
-          flex
-          items-center
-          justify-center
-          bg-green-500
-          py-4
-          drop-shadow-md
+          absolute 
+          transition 
+          opacity-0 
+          rounded-full 
+          flex 
+          items-center 
+          justify-center 
+          bg-green-500 
+          p-4 
+          drop-shadow-md 
           right-5
-          group-hover:opacity-100
+          group-hover:opacity-100 
           hover:scale-110
         "
       >
